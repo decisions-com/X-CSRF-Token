7576fccde27b80a178039683c6066af8e0a70c74
Jon Hallam <Jon@decisions.com>
2020-08-06 14:36:02 +0100

    Initial commit

---
+++ X-CSRF-Token/Steps/CustomAsyncStep.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// </summary>
+namespace X-CSRF-Token.Steps
+{
+    class CustomAsyncStep
+    {
+    }
+}
